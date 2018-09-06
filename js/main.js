9a836c16ea9fd4ef349427099160a551c5f583d4
Lakeshia Williams <38958988+FENDdeveloperkei@users.noreply.github.com>
2018-09-06 17:33:14 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -158,28 +158,36 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
-
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
   li.append(name);
+  
+  const image = document.createElement('img');
+  image.className = 'restaurant-img';
+  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name} restaurant`;
+  li.append(image);
+  
+  const infoDiv = document.createElement('div');
+  infoDiv.className = 'info-Div';
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  infoDiv.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  infoDiv.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.tabIndex = '3';
+  
+  infoDiv.append(more);
 
+  li.append(infoDiv);
+  
   return li
 }
 
@@ -198,14 +206,14 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 
 } 
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
+
+// Service Worker
+if('serviceWorker' in navigator) {
+window.addEventListener('load', () => {
+  navigator.serviceWorker
+  .register('/sw.js')
+  .then(reg => console.log('Service Worker: Registered'))
+  .catch(err => console.log(`Service Worker: Error: ${err}`))
+})
+}
 
